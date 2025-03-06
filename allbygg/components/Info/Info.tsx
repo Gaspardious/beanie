b53312bc2587670772460b95461a0ad07f050be1
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2025-03-06 08:38:00 +0100

    Small changes

--- allbygg/components/Info/Info.tsx
+++ allbygg/components/Info/Info.tsx
@@ -15,10 +15,10 @@ const Info: React.FC<InfoProps> = ({ BigText, ImageSrc, SmallText, reverse = fal
         <section className='bg-red-400 h-full w-1/2 text-black'>
             <Image src={ImageSrc} alt="logo" width={1000} height={700} className="object-cover w-full h-auto"/>
         </section>
-            <article className=' h-full w-1/2 text-black flex flex-col items-center justify-center text-center' >
-                <h2 className='pb-10 text-2xl sm:text-4xl md:text-6xl lg:text-8xl'>{BigText}</h2>
-                <p className=' text-1xl sm:text-2xl'>{SmallText}</p>
-            </article>
+          <article className=' h-full w-1/2 text-black flex flex-col items-center justify-center text-center'>
+              <h2 className='pb-10 text-2xl sm:text-4xl md:text-6xl lg:text-8xl'>{BigText}</h2>
+              <p className=' text-1xl sm:text-2xl'>{SmallText}</p>
+          </article>
     </div>
   )
 }
