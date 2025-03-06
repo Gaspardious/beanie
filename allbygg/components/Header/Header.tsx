649beefa1a7ae35f25b132b27672dbba8e14ac80
Dejan Gaspar <107142760+Gaspardious@users.noreply.github.com>
2025-03-06 16:53:44 +0100

    Header fixed

--- allbygg/components/Header/Header.tsx
+++ allbygg/components/Header/Header.tsx
@@ -1,18 +1,34 @@
 'use client'
 import Link from "next/link"
 import Image from "next/image"
+import { useState, useEffect } from "react"
 
 const Header = () => {
+const [isScrolled, setIsScrolled] = useState(false)
 
+useEffect(() => {
+  const onScrolling = () => {
+    if (window.scrollY > 50) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
 
+  window.addEventListener("scroll", onScrolling);
+  return () => window.removeEventListener("scroll", onScrolling);
+}, []);
 
   return (
-    <header className="bg-white h-28 flex flex-col items-center justify-center">
+    <header 
+      className={`h-28 flex flex-col items-center justify-center sticky top-0 z-50 transition-all duration-300 ${
+      isScrolled ? "bg-black/50 backdrop-blur-md" : "bg-white"
+    }`}>
 
       <section className="flex flex-row items-start gap-20 justify-between w-full">
         <div className="flex flex-row items-center justify-center h-full gap-20">
-          <Image src="/logo2.png" alt="logo" width={110} height={110}  />
-          <nav className=" hidden lg:flex gap-10 text-[#263e57] font-bold text-[1.2rem]">
+          <Image src={isScrolled ? "/logo_light.png" : "/logo_dark.png"} alt="logo" width={110} height={110}  />
+          <nav className= {`hidden lg:flex gap-10 font-bold text-[1.2rem] ${isScrolled ? "text-[#ffffff]" : "text-[#263e57] "}`}>
               <Link className="hover:text-[#4c6cd3]" href="/">HEM</Link>
               <Link className="hover:text-[#4c6cd3]" href="/about">OM OSS</Link>
               <Link className="hover:text-[#4c6cd3]" href="/services">TJÄNSTER</Link>
@@ -22,7 +38,7 @@ const Header = () => {
         <div className="flex flex-row items-center justify-center h-full">
           <Link href="/contact"><button className=" hidden sm:bg-gray-500 hover:bg-[#263e90] cursor-pointer text-white font-bold py-2 px-4 rounded mr-10 sm:block ">E-handel</button></Link>
           <Link href="/contact"><button className=" bg-[#263e57] hover:bg-[#263e90] cursor-pointer text-white font-bold py-2 px-4 rounded w-31 mr-10">Begär offert</button></Link>
-          <Image src="/menu.svg" alt="logo" width={25} height={25} className="mr-10 lg:hidden"/>
+          <Image  src={isScrolled ? "/menu_light.svg" : "/menu_dark.svg"} alt="logo" width={25} height={25} className="mr-10 lg:hidden"/>
 
         </div>
       </section>
--- allbygg/components/Info/Info.tsx
+++ allbygg/components/Info/Info.tsx
@@ -13,7 +13,7 @@ const Info: React.FC<InfoProps> = ({ BigText, ImageSrc, SmallText, reverse = fal
   return (
     <div className={`flex flex-row mt-2 items-center justify-center w-full relative bg-white ${reverse ? 'flex-row-reverse' : 'flex-row'}`}>
         <section className='bg-red-400 h-full w-1/2 text-black'>
-            <Image src={ImageSrc} alt="logo" width={1000} height={700} className="object-cover w-full h-auto"/>
+            <Image src={ImageSrc} alt="logo" width={1000} height={1000} className="object-cover w-full h-auto"/>
         </section>
           <article className=' h-full w-1/2 text-black flex flex-col items-center justify-center text-center'>
               <h2 className='pb-10 text-2xl sm:text-4xl md:text-6xl lg:text-8xl'>{BigText}</h2>
